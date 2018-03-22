a95eadf8273ac562fc279918f11979701060f6ca
Shahid <shahidkanwal234@gmail.com>
2018-03-22 09:17:00 -0700

    successfull login from DB.

--- app/src/main/java/com/facefive/meetbook/LoginActivity.java
+++ app/src/main/java/com/facefive/meetbook/LoginActivity.java
@@ -3,7 +3,6 @@ package com.facefive.meetbook;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,7 +15,6 @@ import java.util.Map;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
-import com.android.volley.Request.Method;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
