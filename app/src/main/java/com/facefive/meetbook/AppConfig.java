a1b55e92e805058d808c0ea9f893f4992e633ea5
Shahid <shahidkanwal234@gmail.com>
2018-03-22 12:10:57 -0700

    successfull login from DB.

--- app/src/main/java/com/facefive/meetbook/AppConfig.java
+++ app/src/main/java/com/facefive/meetbook/AppConfig.java
@@ -7,6 +7,8 @@ package com.facefive.meetbook;
 public class AppConfig {
     public static String URL_LOGIN = "http://meetbookapp.com/APIs/login_user.php";
 
+    public static String URL_SIGNUP = "http://meetbookapp.com/APIs/register_user.php";
+
     // Server user register url
 
 }
--- app/src/main/java/com/facefive/meetbook/SignupActivity.java
+++ app/src/main/java/com/facefive/meetbook/SignupActivity.java
@@ -20,6 +20,17 @@ import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class SignupActivity extends AppCompatActivity {
 
     Button next_btn ;
@@ -28,6 +39,9 @@ public class SignupActivity extends AppCompatActivity {
     EditText email_et;
     EditText con_pass_et;
     EditText pass_et;
+    Spinner uni;
+   final String SU_NAME="name",SU_EMAIL="email",SU_PASS="password",SU_UNINAME="uniname";
+    String su_name,su_email,su_pass,su_conpass,su_uniname;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,7 +51,7 @@ public class SignupActivity extends AppCompatActivity {
         email_et= (EditText)findViewById(R.id.email_signupET);
         pass_et= (EditText)findViewById(R.id.pass_signupET);
         con_pass_et= (EditText)findViewById(R.id.confPass_signupET);
-
+        uni=(Spinner)findViewById(R.id.sp_uni);
 
         next_btn= (Button)findViewById(R.id.btn_su_next);
         //dp_iv= (ImageView)findViewById(R.id.iv_cdp_img);
@@ -54,33 +68,35 @@ public class SignupActivity extends AppCompatActivity {
         next_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = name_et.getText().toString();
-                String email = email_et.getText().toString();
-                String pass = pass_et.getText().toString();
-                String con_pass = con_pass_et.getText().toString();
+                 su_name = name_et.getText().toString().trim();
+                 su_email = email_et.getText().toString().trim();
+               su_pass = pass_et.getText().toString().trim();
+                 su_conpass = con_pass_et.getText().toString().trim();
+                 su_uniname=uni.getSelectedItem().toString().trim();
+
                 boolean flag= true;
                 int redColor =getResources().getColor(R.color.colorError);
-                if(name.equals(""))
+                if(su_name.equals(""))
                 {
                     name_et.setHint("Invalid Name");
                     name_et.setHintTextColor(redColor);
                     flag= false;
                 }
-                if(!email.toLowerCase().equals("test@meetbook.com"))
+                if(!su_email.toLowerCase().equals("test@mba"))
                 {
                     email_et.setText("");
                     email_et.setHint("Invalid Email");
                     email_et.setHintTextColor(redColor);
                     flag= false;
                 }
-                if(pass.equals("") || pass.length() < 4)
+                if(su_pass.equals("") || su_conpass.length() < 4)
                 {
                     pass_et.setText("");
                     pass_et.setHint("Invalid Pass");
                     pass_et.setHintTextColor(redColor);
                     flag= false;
                 }
-                if(!pass.equals(con_pass))
+                if(!su_pass.equals(su_conpass))
                 {
                     con_pass_et.setText("");
                     con_pass_et.setHint("Password Not Match");
@@ -90,9 +106,11 @@ public class SignupActivity extends AppCompatActivity {
 
                 if(flag) {
                     Intent i = new Intent(getApplicationContext(), VerifyCodeActivity.class);
-                    i.putExtra("flow", "onSignUp");
-                    i.putExtra("email_key" , email);
-                    startActivity(i);
+                    //i.putExtra("flow", "onSignUp");
+                   // i.putExtra("email_key" , email);
+                    signUP();
+
+                   // startActivity(i);
 
                 }
 //                Intent i = new Intent(getApplicationContext(), VerifyCodeActivity.class);
@@ -105,6 +123,45 @@ public class SignupActivity extends AppCompatActivity {
 
 
     }
+
+    public void signUP(){
+
+        RequestQueue requestQueue= Volley.newRequestQueue(this);
+
+        StringRequest stringRequest=new StringRequest(Request.Method.POST, AppConfig.URL_SIGNUP, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+
+                Toast.makeText(SignupActivity.this,response,Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(SignupActivity.this,HomeActivity.class);
+                startActivity(intent);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(SignupActivity.this,error.toString(),Toast.LENGTH_SHORT).show();
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+
+                Map<String, String> params=new HashMap<String, String>();
+                params.put(SU_NAME,su_name);
+                params.put(SU_EMAIL,su_email);
+                params.put(SU_PASS,su_pass);
+                params.put(SU_UNINAME,su_uniname);
+
+                return params;
+            }
+        };
+        requestQueue.add(stringRequest);
+
+    }
+
+
+
+
    /* public View.OnClickListener btnChoosePhotoPressed = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
